2335dbd0e765a89d5424260f631ba5da1895106a
Favour Ibude <queenfavi@gmail.com>
2023-12-17 05:54:24 +0100

    config.yaml

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from objectClassifier import logger
 
-logger.info("welocme to my custom log")
+#logger.info("welocme to my custom log")
+
+from objectClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
